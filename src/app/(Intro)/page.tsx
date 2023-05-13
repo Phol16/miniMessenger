9887cbea9a4d4d8bf00092c44169c1d4c355e52e
Phol <Pholibertlim@gmail.com>
2023-05-14 00:30:47 +0800

    change intro photo src

--- src/app/(Intro)/page.tsx
+++ src/app/(Intro)/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import AuthForm from './components/AuthForm';
+import image from '../../../public/photo.png'
 
 export default function Intro() {
   return (
@@ -18,7 +19,7 @@ export default function Intro() {
       </aside>
       <aside className='max-w-xl'>
         <Image
-          src='https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.8562-6/120973513_338186077283942_8148888802958728934_n.png?_nc_cat=1&ccb=1-7&_nc_sid=6825c5&_nc_ohc=SkrugJTOEVMAX865AQz&_nc_ht=scontent.fdvo2-2.fna&oh=00_AfBZgOUAZSw-w8_UGQcSxDKJALr1K7OXFnENMS75AO8llQ&oe=645DC9A7'
+          src={image}
           alt='messengerPhoto'
           width={700}
           height={0}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
+import logo from '../../public/logo.png'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-8 max-w-6xl m-auto'>
       <Image
-        src={`https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.8562-6/120009688_325579128711709_1736249742330805861_n.png?_nc_cat=1&ccb=1-7&_nc_sid=6825c5&_nc_ohc=bVT03qrA8Z4AX-_9SpK&_nc_ht=scontent.fdvo2-2.fna&oh=00_AfA3pebDzMLlioQrHRZYbOu_1fJX7ilIzuFifJXwsiibrA&oe=645E107D`}
+        src={logo}
         alt='MessengerLogo'
         width={40}
         height={0}
